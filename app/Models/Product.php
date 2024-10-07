a2478ab82cdcd3860c5cad87f8f39f558dbda1be
AetiusDev <d.alexandruhoria@yahoo.com>
2024-10-07 17:44:57 +0300

    Solution

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function goodReviews(): HasMany
+    {
+        return $this->reviews()->where('rating', '>=', 4);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Review;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +22,20 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $products = Product::factory()->count(10)->create();
+        $categories = Category::factory()->count(5)->create();
+
+        foreach ($products as $product) {
+            $product->categories()->attach($categories->random(rand(1, 3)));
+
+            $reviewCount = rand(3, 5);
+
+            for ($i = 0; $i < $reviewCount; $i++) {
+                $product->reviews()->save(new Review([
+                    'rating' => rand(2, 5),
+                ]));
+            }
+        }
     }
 }
